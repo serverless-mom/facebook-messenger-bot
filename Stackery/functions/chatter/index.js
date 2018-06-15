d753b0eb9657561c680f52ed7ee1c58e3e6747fd
tobyfee@stackery.io <tobyfee@stackery.io>
2018-06-15 17:43:59 +0000

    Modified 1 node:
    Updated function node chatter

--- Stackery/functions/chatter/index.js
+++ Stackery/functions/chatter/index.js
@@ -1,5 +1,29 @@
-module.exports = function handler(message, context, callback) {
-  console.dir(message);
-
-  callback(null, {});
+'use strict';
+var VERIFY_TOKEN = "my_awesome_token";
+exports.handler = (event, context, callback) => {
+    
+  // process GET request
+  if(event.queryStringParameters){
+    var queryParams = event.queryStringParameters;
+ 
+    var rVerifyToken = queryParams['hub.verify_token']
+ 
+    if (rVerifyToken === VERIFY_TOKEN) {
+      var challenge = queryParams['hub.challenge']
+      
+      var response = {
+        'body': parseInt(challenge),
+        'statusCode': 200
+      };
+      
+      callback(null, response);
+    }else{
+      var response = {
+        'body': 'Error, wrong validation token',
+        'statusCode': 422
+      };
+      
+      callback(null, response);
+    }
+  }
 }
